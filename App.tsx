51ad0cf409c60057761c0b059843996fdbf17594
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-11-17 17:12:12 -0300

    feat: configs iniciais

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Groups from "./src/screens/Groups";
+
+export default function App() {
+  return <Groups />;
+}
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
